04f352f058fb45158d467cda941ff8310eba64d4
André Garcia Marques <gmarques.andre@hotmail.com>
2022-06-02 13:54:41 -0300

    add item type enum

---
+++ FinanceApp.Shared/Enum/EItemType.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApp.Shared.Enum
+{
+    public enum EItemType
+    {
+        [Display(Name = "Gasto")]
+        Spending = 0,
+        [Display(Name = "Renda")]
+        Income = 1,
+        [Display(Name = "Empréstimos/Financiamentos")]
+        Loan = 2,
+        [Display(Name = "Renda Fixa")]
+        PrivateFixedIncomeInvestment = 3,
+        [Display(Name = "Tesouro Direto")]
+        TreasuryBond = 4,
+        [Display(Name = "FGTS")]
+        FGTS = 5,
+        [Display(Name = "Conta Corrente")]
+        CurrentBalance = 6,
+
+    }
+}
