0c6ff4fd79d3d516ec7d748afe0bce7b8f739410
André Garcia Marques <gmarques.andre@hotmail.com>
2022-06-06 20:36:31 -0300

    boa

---
+++ FinanceApp/Controllers/DataController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FinanceApp.Core.Services.DataServices;
+
+namespace FinanceApp.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        public readonly IDataService _service;
+
+        public DataController(IDataService service)
+        {
+            _service = service;
+        } 
+
+        [HttpGet("GetProspectIndexes")]
+        public async Task<IActionResult> GetProspectIndexesAsync()
+        {
+            var values = await _service.GetIndexesProspect();
+            return Ok(values);
+        }
+
+        //[HttpGet("GetHolidays")]
+        //public IActionResult GetHolidays()
+        //{
+        //    DateTime lastUpdateDateLocal = _context.Holidays.Select(a => a.DateLastUpdate).Max();
+
+        //    var values = _context.Holidays.ToList();
+        //    return Ok(values);
+
+        //}
+
+        //[HttpGet("GetWorkingDaysByYear")]
+        //public IActionResult GetWorkingDaysByYear()
+        //{
+
+        //    var values = _context.WorkingDaysByYear.ToList();
+        //    return Ok(values);
+
+        //}
+
+    }
+}
