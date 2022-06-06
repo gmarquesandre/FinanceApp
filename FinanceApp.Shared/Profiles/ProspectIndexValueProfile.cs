5ab69e5c59cee15497c308e8462e25f9dc234e42
André Garcia Marques <gmarques.andre@hotmail.com>
2022-06-06 20:45:03 -0300

    add prospect index profile

---
+++ FinanceApp.Shared/Profiles/ProspectIndexValueProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FinanceApp.Shared.Dto;
+
+namespace FinanceApp.Shared.Profiles
+{
+    public class ProspectIndexValue : Profile
+    {
+        public ProspectIndexValue()
+        {
+            CreateMap<ProspectIndexValue, ProspectIndexValueDto>();
+            CreateMap<ProspectIndexValueDto, ProspectIndexValue>();
+        }
+    }
+}
