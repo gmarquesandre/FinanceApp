91992380f1dfd400c14d18a8fb1a357851a04004
André Garcia Marques <gmarques.andre@hotmail.com>
2022-06-07 00:49:04 -0300

    add index cach dictionary

---
+++ FinanceApp.Shared/Enum/EDataCacheKey.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace FinanceApp.Shared.Enum
+{
+    public enum EDataCacheKey
+    {
+        [Description("indexes")]
+        Indexes = 0,
+        [Description("indexesProspect")]
+        IndexesProspect = 1,
+        [Description("treasuryBond")]
+        TreasuryBond = 2,
+        [Description("holidays")]
+        Holidays = 4,
+        [Description("workingDays")]
+        WorkingDays = 5,
+        [Description("assets")]
+        Asset = 6,
+        [Description("investmentFund")]
+        InvestmentFund = 7
+    }
+}
