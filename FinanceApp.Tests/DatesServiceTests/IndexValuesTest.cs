43687b42256a8e2fa53792800f059aefe6a8883f
André Garcia Marques <gmarques.andre@hotmail.com>
2022-06-10 01:36:17 -0300

    add tests to future values

--- FinanceApp.Tests/DatesServiceTests/IndexValuesTest.cs
+++ FinanceApp.Tests/DatesServiceTests/IndexValuesTest.cs
@@ -1,9 +1,11 @@
 using FinanceApp.Core.Importers;
 using FinanceApp.Core.Services;
 using FinanceApp.Shared.Enum;
+using FinanceApp.Shared.Models.CommonTables;
 using FinanceApp.Tests.Base;
 using Microsoft.Extensions.Caching.Memory;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Threading.Tasks;
 using Xunit;
@@ -62,6 +64,76 @@ namespace FinanceApp.Tests.DatesServiceTests
             Assert.True(value3.ToString("N", setPrecision) == 1.01492840.ToString("N", setPrecision));
             
         }
+
+
+        [Fact]
+        public async Task MustCalculateSelicValuesWithFutureValues()
+        {
+            var mapper = GetConfigurationIMapper();
+            var context = await CreateFinanceContext();
+
+            //Instancias 
+            MemoryCacheOptions cacheOptions = new();
+
+            var memoryCache = new MemoryCache(cacheOptions);
+
+            var datesService = new DatesService(context,
+                                                mapper,
+                                                memoryCache);
+
+
+            var indexService = new IndexService(context,
+                                                mapper,
+                                                memoryCache,
+                                                datesService);
+
+            var holidaysImporter = new HolidaysImporter(context);
+
+            //Importar dados para comparação
+
+            await holidaysImporter.GetHolidays(2022, 2022);
+            
+            //Importar dados para comparação
+
+            context.ProspectIndexValues.Add(new ProspectIndexValue()
+            {
+                Average = 0,
+                Median = 12.649937272,
+                BaseCalculo = 0,
+                DateEnd = new DateTime(2022, 6, 1),
+                DateStart = new DateTime(2022, 5, 5),
+                Index = EIndex.Selic,
+                IndexRecurrence = EIndexRecurrence.Yearly,
+            });
+            context.WorkingDaysByYear.Add(new WorkingDaysByYear() {
+                WorkingDays = 252,
+                Year = 2022
+            });
+            context.IndexValues.AddRange(new List<IndexValue>() {
+                new IndexValue()
+                {
+                    Index = EIndex.Selic,
+                    IndexRecurrence = EIndexRecurrence.Daily,
+                    Date = new DateTime(2022,3,17),
+                    DateEnd = new DateTime(2022,3,17),
+                    Value = 0.00043739                    
+                },
+            });
+
+            context.SaveChanges();
+
+            var value = await indexService.GetIndexValueBetweenDates(EIndex.Selic, new DateTime(2022, 3, 17), new DateTime(2022, 6, 1));
+
+            NumberFormatInfo setPrecision = new();
+
+            setPrecision.NumberDecimalDigits = 6;
+
+            //Valores extaidos da calculadora do cidadão para CDI de 100% https://www3.bcb.gov.br/CALCIDADAO/publico/exibirFormCorrecaoValores.do?method=exibirFormCorrecaoValores&aba=5
+            Assert.True(value.ToString("N", setPrecision) == 1.02368780.ToString("N", setPrecision));
+
+        }
+
+
 
 
         [Fact]
