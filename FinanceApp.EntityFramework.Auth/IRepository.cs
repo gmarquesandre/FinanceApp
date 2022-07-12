170bda475a1c700af481ef662d5b312527f026f0
Andre <gmarques.andre@hotmail.com>
2022-07-11 23:57:19 -0300

    add functions

--- FinanceApp.EntityFramework.Auth/IRepository.cs
+++ FinanceApp.EntityFramework.Auth/IRepository.cs
@@ -5,6 +5,10 @@ namespace FinanceApp.EntityFramework
     public interface IRepository<TEntity> where TEntity : UserTable
     {
         Task<TEntity> InsertAsync(TEntity entity);
-        TEntity Update(TEntity model);
+        TEntity Update(int id, TEntity entity);
+        Task<TEntity> FirstOrDefaultAsync();
+        void Remove(TEntity entity);
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity> GetById(int id);
     }
 }
--- FinanceApp.EntityFramework.Auth/Repository.cs
+++ FinanceApp.EntityFramework.Auth/Repository.cs
@@ -1,6 +1,7 @@
 ﻿using FinanceApp.Shared;
 using FinanceApp.Shared.Models.UserTables;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinanceApp.EntityFramework
 {
@@ -16,18 +17,50 @@ namespace FinanceApp.EntityFramework
         public async Task<TEntity> InsertAsync(TEntity entity)
         {
             entity.UserId = _httpContextAccessor.HttpContext.User.GetUserId();
+            entity.CreationDateTime = DateTime.Now;
+            entity.UpdateDateTime = DateTime.Now;
             var newRow = (await _context.Set<TEntity>().AddAsync(entity)).Entity;
             await _context.SaveChangesAsync();
             return newRow;
         }
 
-        public TEntity Update(TEntity entity)
+        public TEntity Update(int id, TEntity entity)
         {
+            entity.Id = id;
             entity.UserId = _httpContextAccessor.HttpContext.User.GetUserId();
+            entity.UpdateDateTime = DateTime.Now; 
             var updatedRow = _context.Set<TEntity>().Update(entity).Entity;
             _context.SaveChanges();
             return updatedRow;
 
+        }
+
+        public async Task<TEntity> FirstOrDefaultAsync()
+        {
+            var item = (await _context.Set<TEntity>().AsNoTracking().FirstOrDefaultAsync());
+
+            return item;
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            var items = await _context.Set<TEntity>().AsNoTracking().ToListAsync();
+
+            return items;
+        }
+        public void Remove(TEntity entity)
+        {
+
+            _context.Set<TEntity>().Remove(entity);
+
+            _context.SaveChanges();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            var item = await _context.Set<TEntity>().AsNoTracking().FirstOrDefaultAsync(a => a.Id == id);
+
+            return item;
         }
     }
 }
